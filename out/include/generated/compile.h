ced1407d1e0272b861607fdb9d4363031f1a2e6c
TrimedHunter <trimedhunter@gmail.com>
2024-08-17 17:29:52 +0800

    New:Sound Control

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#3 SMP PREEMPT Sat Aug 17 16:10:14 CST 2024"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Aug 17 16:50:01 CST 2024"
 #define LINUX_COMPILE_BY "trimedhunter"
 #define LINUX_COMPILE_HOST "trimedhunter-VMware-Virtual-Platform"
 #define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
