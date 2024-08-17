b4ec38ce6256ec706e573c3870e33ae6736b8475
TrimedHunter <trimedhunter@gmail.com>
2024-08-17 16:22:49 +0800

    Fix:DC Kernel Flashing

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 3 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#3 SMP PREEMPT Sat Aug 17 16:10:14 CST 2024"
+#define LINUX_COMPILE_BY "trimedhunter"
+#define LINUX_COMPILE_HOST "trimedhunter-VMware-Virtual-Platform"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
