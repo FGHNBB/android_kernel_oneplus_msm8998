04a83dbe90f7c051b70b53b0057c9f3852625760
Trimedhunter <trimedhunter@gmail.com>
2024-08-18 12:24:59 +0800

    qpnp-fg-gen3: allow write any learned capacity

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -4096,7 +4096,8 @@ static int fg_psy_set_property(struct power_supply *psy,
 		}
 		break;
 	case POWER_SUPPLY_PROP_CHARGE_FULL:
-		if (chip->cl.active) {
+/*
+		if (chip->cl->active) {
 			pr_warn("Capacity learning active!\n");
 			return 0;
 		}
@@ -4104,6 +4105,7 @@ static int fg_psy_set_property(struct power_supply *psy,
 			pr_err("charge_full is out of bounds\n");
 			return -EINVAL;
 		}
+*/
 		chip->cl.learned_cc_uah = pval->intval;
 		rc = fg_save_learned_cap_to_sram(chip);
 		if (rc < 0)
