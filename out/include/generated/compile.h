f2e54e8b03f0c27969938cf51d6569e847528613
Trimedhunter <trimedhunter@gmail.com>
2024-08-20 10:09:55 +0800

    reverse: -O3, LTO, F2FS

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Aug 18 13:21:19 CST 2024"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 18 15:52:22 CST 2024"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "localhost"
 #define LINUX_COMPILER "Ubuntu clang version 14.0.0-1ubuntu1.1"
