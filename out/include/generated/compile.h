c02f82c070855401d5b1b7962f92f30a5d2058f9
Trimedhunter <trimedhunter@gmail.com>
2024-08-24 22:42:25 +0800

    nothing is here beacuse nothing is needed to be said.

--- out/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 18 15:52:22 CST 2024"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "localhost"
-#define LINUX_COMPILER "Ubuntu clang version 14.0.0-1ubuntu1.1"
